64735d3109fc9b9f4a2792c9a4b610fe6b5274de
marievy <ivymarieporras@gmail.com>
2024-11-16 18:40:20 -0700

    feat: add portfolio greeting at home page

--- js/script.js
+++ js/script.js
@@ -9,6 +9,23 @@ document.addEventListener("DOMContentLoaded", function () {
    console.log("Oh! Hello there! I'm Ivy Porras");
    console.log("Today's date is:", currentDate.toString());
 
+   // ----- GREETING 
+   const currentHour = new Date().getHours();
+   const greetingElement = document.getElementById("greeting");
+
+   if (greetingElement) {
+      if (currentHour < 12) {
+         greetingElement.textContent = "Good morning!";
+         greetingElement.classList.add("morning");
+      } else if (currentHour < 17) {
+         greetingElement.textContent = "Good afternoon!";
+         greetingElement.classList.add("afternoon");
+      } else {
+         greetingElement.textContent = "Good evening!";
+         greetingElement.classList.add("evening");
+      }
+   }
+
 });
 
 
